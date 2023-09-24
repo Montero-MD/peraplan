38e21711bb5f05e145fb988576b745522aeacb58
Matthew Montero <99379541+Montero-MD@users.noreply.github.com>
2023-09-24 22:30:29 +0800

    modified formatter for amount and date textfields

--- lib/components/perain_section.dart
+++ lib/components/perain_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:peraplan/pages/home_page.dart';
 import 'package:peraplan/utils/styles.dart';
+import 'package:pattern_formatter/pattern_formatter.dart';
 
 class PeraIn extends StatefulWidget {
   const PeraIn({super.key});
@@ -11,6 +11,8 @@ class PeraIn extends StatefulWidget {
 }
 
 class _PeraInState extends State<PeraIn> {
+  final _formkey = GlobalKey<FormState>();
+
   @override
   void setState(VoidCallback fn) {
     super.setState(fn);
@@ -42,141 +44,168 @@ class _PeraInState extends State<PeraIn> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Pera In', style: pIn),
-            Row(
-              children: [
-                Flexible(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                            color: white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: background, width: 1),
+      child: Form(
+        key: _formkey,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Pera In', style: pIn),
+              Row(
+                children: [
+                  Flexible(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                              color: white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: background, width: 1),
+                            ),
                           ),
-                        ),
-                        TextField(
-                          keyboardType: const TextInputType.numberWithOptions(
-                              decimal: true),
-                          textAlign: TextAlign.center,
-                          cursorColor: green,
-                          style: inpAmt,
-                          decoration: InputDecoration(
-                            hintText: 'Enter Amount...',
-                            hintStyle: hintAmt,
-                            border: InputBorder.none,
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Invalid/Null Amount entered';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            cursorColor: green,
+                            style: inpAmt,
+                            decoration: InputDecoration(
+                              hintText: 'Enter Amount...',
+                              hintStyle: hintAmt,
+                              border: InputBorder.none,
+                            ),
+                            inputFormatters: [
+                              ThousandsFormatter(allowFraction: true)
+                            ],
                           ),
-                          inputFormatters: [
-                            FilteringTextInputFormatter.allow(
-                                RegExp(r'^\d+(?:,\d{3})*(\.\d{0,2})?$')),
-                          ],
-                        ),
-                      ]),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Date:', style: subPlan),
-                Flexible(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                            color: background,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                                color: Colors.grey.shade300, width: 1),
+                        ]),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('Date:', style: subPlan),
+                  Flexible(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                              color: background,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Colors.grey.shade300, width: 1),
+                            ),
                           ),
-                        ),
-                        TextField(
-                          keyboardType: TextInputType.datetime,
-                          textAlign: TextAlign.center,
-                          cursorColor: hlblue,
-                          style: subPlan,
-                          decoration: InputDecoration(
-                            hintText: 'MM/DD/YYYY',
-                            hintStyle: pText,
-                            border: InputBorder.none,
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Invalid/Null date entered';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              DateInputFormatter(),
+                            ],
+                            textAlign: TextAlign.center,
+                            cursorColor: hlblue,
+                            style: subPlan,
+                            decoration: InputDecoration(
+                              hintText: 'MM/DD/YYYY',
+                              hintStyle: pText,
+                              border: InputBorder.none,
+                            ),
                           ),
-                        ),
-                      ]),
-                ),
-                SizedBox(width: small),
-                Text('Time:', style: subPlan),
-                Flexible(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                            color: background,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                                color: Colors.grey.shade300, width: 1),
+                        ]),
+                  ),
+                  SizedBox(width: small),
+                  Text('Time:', style: subPlan),
+                  Flexible(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                              color: background,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Colors.grey.shade300, width: 1),
+                            ),
                           ),
-                        ),
-                        TextField(
-                          keyboardType: TextInputType.datetime,
-                          textAlign: TextAlign.center,
-                          cursorColor: hlblue,
-                          style: subPlan,
-                          decoration: InputDecoration(
-                            hintText: 'hh:mm AM/PM',
-                            hintStyle: pText,
-                            border: InputBorder.none,
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Invalid/Null time entered';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.datetime,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(r'^[0-9:]+$')),
+                            ],
+                            textAlign: TextAlign.center,
+                            cursorColor: hlblue,
+                            style: subPlan,
+                            decoration: InputDecoration(
+                              hintText: 'hh:mm AM/PM',
+                              hintStyle: pText,
+                              border: InputBorder.none,
+                            ),
                           ),
+                        ]),
+                  ),
+                ],
+              ),
+              Text('Category:', style: subPlan),
+              Row(children: <Widget>[
+                Expanded(child: peraIntype("Salary", 1)),
+                Expanded(child: peraIntype("Allowance", 2)),
+              ]),
+              Row(
+                children: <Widget>[
+                  Expanded(child: peraIntype("Investments", 3)),
+                  Expanded(child: peraIntype("Pension", 4)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        if (_formkey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
+                          );
+                        }
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(5),
+                        width: 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(width: 2.5, color: green),
+                            color: green),
+                        child: Icon(
+                          Icons.arrow_outward,
+                          color: white,
+                          size: 35,
                         ),
-                      ]),
-                ),
-              ],
-            ),
-            Text('Category:', style: subPlan),
-            Row(children: <Widget>[
-              Expanded(child: peraIntype("Salary", 1)),
-              Expanded(child: peraIntype("Allowance", 2)),
+                      )),
+                ],
+              )
             ]),
-            Row(
-              children: <Widget>[
-                Expanded(child: peraIntype("Investments", 3)),
-                Expanded(child: peraIntype("Pension", 4)),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) =>
-                            const HomePage(), // Placeholder, to be changed
-                      ));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(10),
-                      padding: const EdgeInsets.all(5),
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(width: 2.5, color: green),
-                          color: green),
-                      child: Icon(
-                        Icons.arrow_outward,
-                        color: white,
-                        size: 35,
-                      ),
-                    )),
-              ],
-            )
-          ]),
+      ),
     );
   }
 }
